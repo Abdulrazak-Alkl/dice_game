b49eefb8eb17f66bb6e90b67b1bcf9613687a039
abdul_razzak_alkel <abdulrazaqalkl@gmail.com>
2016-08-14 18:27:50 +0800

    fix indentation

--- dice.rb
+++ dice.rb
@@ -1,21 +1,21 @@
 class Dice
 
-	attr_reader :number
+  attr_reader :number
 
-	def roll
-		@number = rand(6) + 1
-	end
+  def roll
+    @number = rand(6) + 1
+  end
 
-	def move?
-		@number == 1
-	end
+  def move?
+    @number == 1
+  end
 
-	def remove?
-		@number == 6 || @number == 1
-	end
+  def remove?
+    @number == 6 || @number == 1
+  end
 
-	def show
-		print "#{@number}, "
-	end
+  def show
+    print "#{@number}, "
+  end
 
 end
--- player.rb
+++ player.rb
@@ -1,43 +1,43 @@
 class Player
 
-	attr_reader :cup, :moved, :name
+  attr_reader :cup, :moved, :name
 
-	def initialize(name, dice_number)
-		@name = name
-		@cup = []
-		@moved = []
-		(1..dice_number).each { @cup << Dice.new }
-	end
+  def initialize(name, dice_number)
+    @name = name
+    @cup = []
+    @moved = []
+    (1..dice_number).each { @cup << Dice.new }
+  end
 
-	#
-	# roll all dices in a cup
-	#
-	def roll_dices
-		@cup.each { |dice| dice.roll }
-	end
+  #
+  # roll all dices in a cup
+  #
+  def roll_dices
+    @cup.each { |dice| dice.roll }
+  end
 
-	#
-	# player is a winner if he has an empty cup
-	#
-	def win?
-		@cup.empty?
-	end
+  #
+  # player is a winner if he has an empty cup
+  #
+  def win?
+    @cup.empty?
+  end
 
-	#
-	# move other player dices to cup
-	#
-	def move
-		@cup = @cup + @moved
-		@moved = []
-	end
+  #
+  # move other player dices to cup
+  #
+  def move
+    @cup = @cup + @moved
+    @moved = []
+  end
 
-	#
-	# print all dices in a cup
-	#
-	def show_dices
-		print "Player #{@name}: "
-		@cup.each { |dice| dice.show }
-		puts "\n"
-	end
+  #
+  # print all dices in a cup
+  #
+  def show_dices
+    print "Player #{@name}: "
+    @cup.each { |dice| dice.show }
+    puts "\n"
+  end
 
 end
